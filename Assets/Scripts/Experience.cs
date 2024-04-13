e1d38ff56bacbf3393f7c1c189e85f489a13736b
Stalkishpotato <jdmiller3134@gmail.com>
2024-04-12 20:29:51 -0500

    Added exp script(nothing of value)

---
+++ Assets/Scripts/Experience.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp : MonoBehaviour
+{
+    public int IncEXP;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
