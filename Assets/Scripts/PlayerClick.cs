da9398eef1b7199aeb7f8955dfd170da0b7809d4
Stalkishpotato <jdmiller3134@gmail.com>
2024-04-12 23:53:57 -0500

    Started on player clicker
    Also moved everything under the canvas so unity AI can be used

---
+++ Assets/Scripts/PlayerClick.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerClick : MonoBehaviour
+{
+    public int goldamount;
+
+
+
+    public void clicked()
+    {
+        
+    }
+
+}
